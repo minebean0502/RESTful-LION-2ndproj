497aeb33a9db269f053b2d4f5b92e1ec53c5ba85
changyukim98 <zxiop12@gmail.com>
2024-04-12 14:12:19 +0900

    feat(auth): Member Entity 추가, Jpa 설정

---
+++ src/main/java/com/hppystay/hotelreservation/auth/entity/Member.java
@@ -0,0 +1,24 @@
+package com.hppystay.hotelreservation.auth.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nickname;
+    private String email;
+    private String password;
+    private MemberRole role;
+}
