64d24078db05e48b5414e9aba974c76ac9092352
MinaeCho <gganbbei@gmail.com>
2024-04-15 17:15:55 +0900

    fix(auth):로그인 기능 충돌 수정, 소소한개선

--- src/main/java/com/hppystay/hotelreservation/auth/controller/MemberController.java
+++ src/main/java/com/hppystay/hotelreservation/auth/controller/MemberController.java
@@ -2,13 +2,10 @@ package com.hppystay.hotelreservation.auth.controller;
 
 import com.hppystay.hotelreservation.auth.dto.CreateMemberDto;
 import com.hppystay.hotelreservation.auth.dto.MemberDto;
-<<<<<<< HEAD
 import com.hppystay.hotelreservation.auth.jwt.JwtRequestDto;
 import com.hppystay.hotelreservation.auth.jwt.JwtResponseDto;
-=======
 import com.hppystay.hotelreservation.auth.service.MemberService;
 import lombok.RequiredArgsConstructor;
->>>>>>> main
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
