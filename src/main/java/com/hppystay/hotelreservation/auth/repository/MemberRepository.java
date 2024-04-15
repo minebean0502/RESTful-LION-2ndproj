80e21e1dbbd3b237f462bb5539b58d16de951dde
changyukim98 <zxiop12@gmail.com>
2024-04-15 13:35:35 +0900

    feat(auth): WebSecurityConfig 추가, JwtTokenFilter 추가

--- src/main/java/com/hppystay/hotelreservation/auth/repository/MemberRepository.java
+++ src/main/java/com/hppystay/hotelreservation/auth/repository/MemberRepository.java
@@ -1,4 +1,10 @@
 package com.hppystay.hotelreservation.auth.repository;
 
-public interface MemberRepository {
+import com.hppystay.hotelreservation.auth.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findMemberByEmail(String email);
 }
