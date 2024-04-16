4a89c51cbd5276dc31c1bd18b41d14734487da64
changyukim98 <zxiop12@gmail.com>
2024-04-16 11:29:36 +0900

    feat(auth): OAuth2 네이버 간편 로그인 기능 개발

--- src/main/java/com/hppystay/hotelreservation/view/ViewController.java
+++ src/main/java/com/hppystay/hotelreservation/view/ViewController.java
@@ -1,4 +1,13 @@
 package com.hppystay.hotelreservation.view;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ViewController {
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 }
