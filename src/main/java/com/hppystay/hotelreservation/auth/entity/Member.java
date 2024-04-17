f765568799be2983a1449985cdeddfff184dbd37
changyukim98 <zxiop12@gmail.com>
2024-04-17 11:23:14 +0900

    fix: BaseEntity 중복 제거

--- src/main/java/com/hppystay/hotelreservation/auth/entity/Member.java
+++ src/main/java/com/hppystay/hotelreservation/auth/entity/Member.java
@@ -1,19 +1,20 @@
 package com.hppystay.hotelreservation.auth.entity;
 
-import com.hppystay.hotelreservation.hotel.entity.BaseEntity;
+import com.hppystay.hotelreservation.common.entity.BaseEntity;
 import com.hppystay.hotelreservation.hotel.entity.Reservation;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Entity
-@Builder
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Member extends BaseEntity {
