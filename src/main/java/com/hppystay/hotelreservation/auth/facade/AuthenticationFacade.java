376e8ab6e01ae45c7ba50de0c2e8a46f90beba73
MinaeCho <gganbbei@gmail.com>
2024-04-19 15:35:55 +0900

    facade 추가' df :wq

---
+++ src/main/java/com/hppystay/hotelreservation/auth/facade/AuthenticationFacade.java
@@ -0,0 +1,13 @@
+package com.hppystay.hotelreservation.auth.facade;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFacade {
+    public Authentication getAuth() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+}
