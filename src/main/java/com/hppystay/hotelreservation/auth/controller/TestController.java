dfc1fd6c8f4e316d1a5064a2900b820024e6f59b
changyukim98 <zxiop12@gmail.com>
2024-04-19 17:20:13 +0900

    feat(auth): oauth2 로그인 시 토큰 발급

--- src/main/java/com/hppystay/hotelreservation/auth/controller/TestController.java
+++ src/main/java/com/hppystay/hotelreservation/auth/controller/TestController.java
@@ -3,19 +3,38 @@ package com.hppystay.hotelreservation.auth.controller;
 import com.hppystay.hotelreservation.auth.jwt.JwtTokenUtils;
 import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/test")
 @RequiredArgsConstructor
 public class TestController {
     private final JwtTokenUtils jwtTokenUtils;
+    private final StringRedisTemplate redisTemplate;
 
     @GetMapping("/token")
-    public Claims val(@RequestParam("token") String jwt) {
-        return jwtTokenUtils.parseClaims(jwt);
+    public Claims val(@RequestParam("token") String token) {
+        return jwtTokenUtils.parseClaims(token);
+    }
+
+    @GetMapping("/uuid")
+    public String uuid(@RequestParam("uuid") String uuid) {
+        return redisTemplate.opsForValue().get(uuid);
+    }
+
+    @GetMapping("/redis")
+    public String test() {
+        ValueOperations<String, String> operations
+                = redisTemplate.opsForValue();
+
+        operations.set("토큰", "리프레시 토큰", 10, TimeUnit.SECONDS);
+        return "test";
     }
 }
