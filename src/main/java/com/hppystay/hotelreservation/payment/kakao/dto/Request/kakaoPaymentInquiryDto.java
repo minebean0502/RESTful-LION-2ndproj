56ea934b13c72b78c4768d43fa0bfd0cefdc9161
minebean0502 <no4323@gmail.com>
2024-04-22 10:19:11 +0900

    delete : 카카오 진행 불가로 인한 삭제

--- src/main/java/com/hppystay/hotelreservation/payment/kakao/dto/Request/kakaoPaymentInquiryDto.java
+++
@@ -1,9 +0,0 @@
-package com.hppystay.hotelreservation.payment.kakao.dto.Request;
-
-import lombok.Data;
-
-@Data
-public class kakaoPaymentInquiryDto {
-    private String cid;
-    private String tid;
-}
--- src/main/java/com/hppystay/hotelreservation/payment/kakao/repository/KaKaoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hppystay.hotelreservation.payment.kakao.repository;
-
-import com.hppystay.hotelreservation.payment.kakao.entity.KakaoPayment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KaKaoRepository extends JpaRepository<KakaoPayment, Long> {
-}
