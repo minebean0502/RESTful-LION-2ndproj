a9f0116cebc52bd054d55160851031066eac7827
minebean0502 <no4323@gmail.com>
2024-04-26 15:52:49 +0900

    feat : CustomRequestMatchers 양식에 따라 수정완료

--- src/main/java/com/hppystay/hotelreservation/view/ViewController.java
+++ src/main/java/com/hppystay/hotelreservation/view/ViewController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Slf4j
 @Controller
 public class ViewController {
-    @Value("${TOSS_CLIENT_KEY")
+    @Value("${TOSS_CLIENT_KEY}")
     private String clientKey;
 
     @GetMapping("/login")
@@ -62,10 +62,23 @@ public class ViewController {
     public String oneHotelDetails() {
         return "temphtml/hotelDetails";
     }
+
+    // 호텔 예약 완료(가정) 후 toss 결제용 view
     @GetMapping("/hotel/test")
     public String tossTestHotel1Room1to3(Model model){
         model.addAttribute("clientKey", clientKey);
         return "toss/reservation";
+    }
+
+    // 결제 성공 후 toss redirect view
+    @GetMapping("/hotel/test/paymentComplete")
+    public String tossTestHotelPaySuccess() {
+        return "toss/success";
+    }
+    // 결제 실패 후 toss redirect view
+    @GetMapping("/hotel/test/paymentFail")
+    public String tossTestHotelPayFail() {
+        return "toss/Fail";
     }
 }
 
