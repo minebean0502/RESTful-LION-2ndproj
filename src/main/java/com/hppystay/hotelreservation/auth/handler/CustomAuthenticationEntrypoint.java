f4c5ee2e752f6f46a4e6a94675d85929de20dc43
changyukim98 <zxiop12@gmail.com>
2024-04-29 11:55:41 +0900

    fix(auth): 인증, 인가 실패시 오류 처리 최종

--- src/main/java/com/hppystay/hotelreservation/auth/handler/CustomAuthenticationEntrypoint.java
+++ src/main/java/com/hppystay/hotelreservation/auth/handler/CustomAuthenticationEntrypoint.java
@@ -4,6 +4,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -23,29 +24,18 @@ public class CustomAuthenticationEntrypoint implements AuthenticationEntryPoint 
             HttpServletResponse response,
             AuthenticationException authException
     ) throws IOException, ServletException {
-        // 응답을 JSON 형식으로 반환하기 위해 contentType 설정
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
+        String requestURI = request.getRequestURI();
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
-        // 상태 코드를 401 Unauthorized로 설정
 
-        // 에러 메시지를 JSON 형식으로 변환하여 클라이언트에 반환
-        Map<String, String> errorResponse = new HashMap<>();
-        errorResponse.put("message", "로그인이 필요합니다.");
-        response.getWriter().print(convertMapToJson(errorResponse));
-
-        // request.getRequestDispatcher("/login").forward(request, response);
-    }
-
-    // Map을 JSON 형식의 문자열로 변환하는 메서드
-    private String convertMapToJson(Map<String, String> map) throws IOException {
-        StringBuilder json = new StringBuilder();
-        json.append("{");
-        for (Map.Entry<String, String> entry : map.entrySet()) {
-            json.append("\"").append(entry.getKey()).append("\":\"").append(entry.getValue()).append("\",");
+        if (requestURI.startsWith("/api/")) {
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            // 에러 메시지를 JSON 형식으로 변환하여 클라이언트에 반환
+            Map<String, String> errorResponse = new HashMap<>();
+            errorResponse.put("message", "로그인이 필요합니다.");
+            response.getWriter().print(JSONObject.toJSONString(errorResponse));
+        } else {
+            request.getRequestDispatcher("/login").forward(request, response);
         }
-        json.deleteCharAt(json.length() - 1);
-        json.append("}");
-        return json.toString();
     }
 }
