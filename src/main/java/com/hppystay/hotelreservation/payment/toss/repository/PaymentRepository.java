dbfc0563c7e27503af2369f950eb72862d738c78
MinaeCho <gganbbei@gmail.com>
2024-04-30 17:07:48 +0900

    fix:댓글 수정페이지 수정

--- src/main/java/com/hppystay/hotelreservation/payment/toss/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hppystay.hotelreservation.payment.toss.repository;
-
-import com.hppystay.hotelreservation.payment.toss.entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
--- src/main/java/com/hppystay/hotelreservation/payment/toss/repository/TossPaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hppystay.hotelreservation.payment.toss.repository;
-
-import com.hppystay.hotelreservation.payment.toss.entity.TossPayment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TossPaymentRepository extends JpaRepository<TossPayment, Long> {
-}
