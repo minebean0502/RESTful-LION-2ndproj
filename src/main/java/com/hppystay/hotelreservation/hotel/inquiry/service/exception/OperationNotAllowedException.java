3e72c6fb54b4de4bea5a91c5ab94b6c604c6d081
MinaeCho <gganbbei@gmail.com>
2024-04-30 17:21:18 +0900

    .

--- src/main/java/com/hppystay/hotelreservation/hotel/inquiry/service/exception/OperationNotAllowedException.java
+++
@@ -1,7 +0,0 @@
-package com.hppystay.hotelreservation.hotel.inquiry.service.exception;
-
-public class OperationNotAllowedException extends RuntimeException {
-    public OperationNotAllowedException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/hppystay/hotelreservation/hotel/inquiry/service/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.hppystay.hotelreservation.hotel.inquiry.service.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
